776f5f66ec0da90b6092e555bb6f04c526ad962d
Datlesosad <leedat104517@gmail.com>
2025-11-04 20:49:00 +0700

    feat(api-gateway): update routes gateway

--- services/user-company-service/src/routes/index.ts
+++ services/user-company-service/src/routes/index.ts
@@ -2,12 +2,11 @@ import { Router } from "express";
 import userRoutes from "./user.routes";
 import companyRoutes from "./company.routes";
 import authRoutes from "./auth.routes";
-import { isAuthenticated } from "../middlewares";
 
 const router = Router();
 
-router.use("/auth", authRoutes);
-router.use("/users", userRoutes);
-router.use("/companies", companyRoutes);
+router.use(authRoutes);
+router.use(userRoutes);
+router.use(companyRoutes);
 
 export default router;
