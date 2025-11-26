1d1fb6f7a183d220b8836805b6cc15790a3d82cf
khai <khai1108070707@gmail.com>
2025-11-26 14:48:20 +0700

    *fix: fix queue

--- services/post-hire-service/src/worker/interface.ts
+++ services/post-hire-service/src/worker/interface.ts
@@ -1,9 +1,9 @@
 import { Job, DoneCallback, Queue } from 'bull';
 
-// export interface JobHandler {
-//     register(): Promise<Queue>;
-//     handler(job: Job, done: DoneCallback): Promise<void>;
-// }
+export interface JobHandler {
+    register(): Promise<Queue>;
+    handler(job: Job, done: DoneCallback): Promise<void>;
+}
 
 export interface RefreshJobData {
     job?: string;
